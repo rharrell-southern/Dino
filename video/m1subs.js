71737cab72cf0d403e4f06f4a5d55ab6e2b432e4
chrisban <cban@southern.edu>
2014-07-29 10:30:19 -0400

    Updated video and sub files

---
+++ video/m1subs.js
@@ -0,0 +1,74 @@
+var subs = new Array();
+subs[1] = new Array();
+subs[1][1] = [
+                {
+              on: '00:01',
+                 value: 'Dinosaurs are divided into two categories based on their hip structure.',
+                    callback: updateSubtitles
+                },
+                {
+              on: '00:06',
+                 value: 'As you can see these hips have very different bone structures. With some dinosaurs the pubis bone points forward like a lizard’s hip, and on others the pubis points backward like a bird’s hip.',
+                    callback: updateSubtitles
+                },
+                {
+              on: '00:19',
+                 value: 'Each of these hip types share the same bones: the Ilim, Ischium, and Pubis. But keep in mind the direction the pubis points is what makes them stand apart.',
+                    callback: updateSubtitles
+                },
+                {
+              on: '00:30',
+                 value: 'A dinosaur like this Plateosaurus has pubis that points forward which means he belongs to the “Saurichian” category. The word Saurichian means lizard hip. “Saur” means lizard and “ischi” means hip.',
+                    callback: updateSubtitles
+                },
+                {
+              on: '00:46',
+                 value: 'On the other hand, a dinosaur like this Arrhinoceratops has a pubis that points backward, which means he belongs to the “Ornithischian” category. The word Ornithichian means bird hip. “Ornith” means bird and “ischi”, you guessed it, means hip.',
+                    callback: updateSubtitles
+                },
+                {
+              on: '01:03',
+                 value: 'Take a moment to review the names of these bones and what category they belong to before moving on to the next section.',
+                    callback: updateSubtitles
+                },                
+               //BEGIN INTERACTIVE
+                {
+              on: '01:13',
+                 value: 'Interactive 1',
+                    callback: function(obj) { updateSubtitles(obj); toInteractive('exer1'); }
+                  
+                }
+             ];
+  subs[1][2] = [{
+              on: '00:00',
+                 value: 'Classification',
+                    callback: function(obj) { $('#subnav a').removeClass('active'); $('#subnav a[zone="2"]').addClass("active");}
+                  
+                },
+                {
+              on: '00:00',
+                 value: 'Let’s take a step back and look at these hips and dinosaurs in context.',
+                    callback: updateSubtitles
+                },
+                {
+              on: '00:07',
+                 value: 'Here are three Saurischians. We have a Plateosaurus, Cetiosaurus, and the Oviraptor.',
+                    callback: updateSubtitles
+                },
+                {
+              on: '00:22',
+                 value: 'On the Ornithischians side we have the Arrhinoceratops, Dryosaurus, and the Ankylosaurus.',
+                    callback: updateSubtitles
+                },    
+                {
+              on: '00:33',
+                 value: 'Review these dinosaurs and be sure you know what hips belong to them.',
+                    callback: updateSubtitles
+                },
+                {
+              on: '00:39',
+                 value: 'Interactive 2',
+                    callback: function(obj) { updateSubtitles(obj); toInteractive('exer3'); }
+                  
+                }
+                ];
